cfc07778b0bc6a4a74b39f545e6a15fc391be011
Kernery <kernerylimited@gmail.com>
2025-09-11 15:26:22 +0100

    remove: Remove getColSpan function since abstract form for getting option is not ready

--- form/src/Traits/CanSpanColumns.php
+++ form/src/Traits/CanSpanColumns.php
@@ -17,38 +17,4 @@ class CanSpanColumns
     {
         return $this->colspan;
     }
-
-    public function getColumnSpan(int | string | null $breakpoint = null)
-    {
-        $columnSpan = [];
-
-        $span = $this->getOption('colspan');
-
-        $parent = $this->getParent();
-
-        if ($span === 'full') {
-            $parentSpan = $parent->getColumns();
-
-            if ($breakpoint !== null) {
-                $span = $parentSpan[$breakpoint] ?? null;
-            }
-        }
-
-        if (! is_array($span)) {
-            $span = [
-                'lg' => ceil(12 / ((int) $parent->getColumns('lg')) * $span),
-            ];
-        }
-
-        $columnSpan = [
-            ...$columnSpan,
-            ...$span,
-        ];
-
-        if ($breakpoint !== null) {
-            return $columnSpan[$breakpoint] ?? null;
-        }
-
-        return array_map(fn ($value) => $value * $span, $parent->getColumns());
-    }
 }
